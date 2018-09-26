d2ea16d407878c4063b1bbef80e39fe12dd95f6f
Kay Gawlik <kay.gawlik@charite.de>
2018-09-26 12:27:18 +0200

    Lizenz hinzugefügt

--- octdata/datastruct/series.h
+++ octdata/datastruct/series.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018 Kay Gawlik <kaydev@amarunet.de> <kay.gawlik@beuth-hochschule.de> <kay.gawlik@charite.de>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <string>
@@ -49,7 +66,7 @@ namespace OctData
 		Octdata_EXPORTS std::size_t bscanCount()                 const { return bscans.size(); }
 
 		Octdata_EXPORTS Laterality getLaterality()               const { return laterality; }
-		Octdata_EXPORTS  void setLaterality(Laterality l)              { laterality = l; }
+		Octdata_EXPORTS void setLaterality(Laterality l)               { laterality = l; }
 
 		Octdata_EXPORTS ScanPattern getScanPattern()             const { return scanPattern; }
 		Octdata_EXPORTS void setScanPattern(ScanPattern p)             { scanPattern = p;    }
